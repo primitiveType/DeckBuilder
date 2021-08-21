781677778735862ce27b146e3dd15cf0b831300e
Arthur <arthur.littlefield@gmail.com>
2021-08-21 10:44:42 -0400

    Fix issues with gitignore, build_library.ps1, and missing csproj files

--- build_library.ps1
+++ build_library.ps1
@@ -1,6 +1,8 @@
 dotnet clean "Library\DeckbuilderLibrary\DeckbuilderLibrary" 
 dotnet build "Library\DeckbuilderLibrary\DeckbuilderLibrary" 
-cp "Library\DeckbuilderLibrary\DeckbuilderLibrary\bin\Debug\netstandard2.0\DeckbuilderLibrary.dll" "App\Assets\Dependencies"
-cp "Library\DeckbuilderLibrary\DeckbuilderLibrary\bin\Debug\netstandard2.0\DeckbuilderLibrary.pdb" "App\Assets\Dependencies"
-cp "Library\DeckbuilderLibrary\Content\bin\Debug\netstandard2.0\Content.dll" "App\Assets\Dependencies"
-cp "Library\DeckbuilderLibrary\Content\bin\Debug\netstandard2.0\Content.pdb" "App\Assets\Dependencies"
+$dependencies = "App\Assets\Dependencies\"
+if (!(Test-Path $dependencies) ){mkdir $dependencies}
+cp "Library\DeckbuilderLibrary\DeckbuilderLibrary\bin\Debug\netstandard2.0\DeckbuilderLibrary.dll"  $dependencies
+cp "Library\DeckbuilderLibrary\DeckbuilderLibrary\bin\Debug\netstandard2.0\DeckbuilderLibrary.pdb" $dependencies
+cp "Library\DeckbuilderLibrary\Content\bin\Debug\netstandard2.0\Content.dll" $dependencies
+cp "Library\DeckbuilderLibrary\Content\bin\Debug\netstandard2.0\Content.pdb" $dependencies
