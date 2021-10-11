69ba019d21fb58a8ae650d61e37e25744713863a
Snapper <learningwithsnapper@gmail.com>
2021-10-11 15:32:13 -0400

    Linux compatability

--- build_library.ps1
+++ build_library.ps1
@@ -1,10 +1,11 @@
-dotnet clean "Library\DeckbuilderLibrary\" 
-dotnet build "Library\DeckbuilderLibrary\" 
-$dependencies = "App\Assets\Dependencies\"
+#!/bin/pwsh
+dotnet clean $(Join-Path "Library" "DeckbuilderLibrary")
+dotnet build $(Join-Path "Library" "DeckbuilderLibrary")
+$dependencies = $(Join-Path "App" "Assets" "Dependencies")
 if (!(Test-Path $dependencies) ){mkdir $dependencies}
-cp "Library\DeckbuilderLibrary\DeckbuilderLibrary\bin\Debug\netstandard2.0\DeckbuilderLibrary.dll"  $dependencies
-cp "Library\DeckbuilderLibrary\DeckbuilderLibrary\bin\Debug\netstandard2.0\DeckbuilderLibrary.pdb" $dependencies
-cp "Library\DeckbuilderLibrary\DeckbuilderLibrary\bin\Debug\netstandard2.0\JsonNet.ContractResolvers.dll"  $dependencies
-cp "Library\DeckbuilderLibrary\Content\bin\Debug\netstandard2.0\Content.dll" $dependencies
-cp "Library\DeckbuilderLibrary\Content\bin\Debug\netstandard2.0\Content.pdb" $dependencies
-cp "Library\DeckbuilderLibrary\DeckbuilderLibrary\bin\Debug\netstandard2.0\HexGrid.dll"  $dependencies
+cp $(Join-Path "Library" "DeckbuilderLibrary" "DeckbuilderLibrary" "bin" "Debug" "netstandard2.0" "DeckbuilderLibrary.dll") $dependencies
+cp $(Join-Path "Library" "DeckbuilderLibrary" "DeckbuilderLibrary" "bin" "Debug" "netstandard2.0" "DeckbuilderLibrary.pdb") $dependencies
+cp $(Join-Path "Library" "DeckbuilderLibrary" "DeckbuilderLibrary" "bin" "Debug" "netstandard2.0" "JsonNet.ContractResolvers.dll") $dependencies
+cp $(Join-Path "Library" "DeckbuilderLibrary" "Content" "bin" "Debug" "netstandard2.0" "Content.dll") $dependencies
+cp $(Join-Path "Library" "DeckbuilderLibrary" "Content" "bin" "Debug" "netstandard2.0" "Content.pdb") $dependencies
+cp $(Join-Path "Library" "DeckbuilderLibrary" "DeckbuilderLibrary" "bin" "Debug" "netstandard2.0" "HexGrid.dll") $dependencies
