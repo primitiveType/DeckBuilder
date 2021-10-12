9c488eac6c1ade0a5626f17140389ad9a6199184
Snapper <learningwithsnapper@gmail.com>
2021-10-12 19:29:08 -0400

    Join-Path -> IO:PATH

--- build_library.ps1
+++ build_library.ps1
@@ -1,11 +1,11 @@
 #!/bin/pwsh
 dotnet clean $(Join-Path "Library" "DeckbuilderLibrary")
 dotnet build $(Join-Path "Library" "DeckbuilderLibrary")
-$dependencies = $(Join-Path "App" "Assets" "Dependencies")
+$dependencies = [IO.PATH]::Combine("App", "Assets", "Dependencies")
 if (!(Test-Path $dependencies) ){mkdir $dependencies}
-cp $(Join-Path "Library" "DeckbuilderLibrary" "DeckbuilderLibrary" "bin" "Debug" "netstandard2.0" "DeckbuilderLibrary.dll") $dependencies
-cp $(Join-Path "Library" "DeckbuilderLibrary" "DeckbuilderLibrary" "bin" "Debug" "netstandard2.0" "DeckbuilderLibrary.pdb") $dependencies
-cp $(Join-Path "Library" "DeckbuilderLibrary" "DeckbuilderLibrary" "bin" "Debug" "netstandard2.0" "JsonNet.ContractResolvers.dll") $dependencies
-cp $(Join-Path "Library" "DeckbuilderLibrary" "Content" "bin" "Debug" "netstandard2.0" "Content.dll") $dependencies
-cp $(Join-Path "Library" "DeckbuilderLibrary" "Content" "bin" "Debug" "netstandard2.0" "Content.pdb") $dependencies
-cp $(Join-Path "Library" "DeckbuilderLibrary" "DeckbuilderLibrary" "bin" "Debug" "netstandard2.0" "HexGrid.dll") $dependencies
+cp $([IO.PATH]::Combine("Library", "DeckbuilderLibrary", "DeckbuilderLibrary", "bin", "Debug", "netstandard2.0", "DeckbuilderLibrary.dll")) $dependencies
+cp $([IO.PATH]::Combine("Library", "DeckbuilderLibrary", "DeckbuilderLibrary", "bin", "Debug", "netstandard2.0", "DeckbuilderLibrary.pdb")) $dependencies
+cp $([IO.PATH]::Combine("Library", "DeckbuilderLibrary", "DeckbuilderLibrary", "bin", "Debug", "netstandard2.0", "JsonNet.ContractResolvers.dll")) $dependencies
+cp $([IO.PATH]::Combine("Library", "DeckbuilderLibrary", "Content", "bin", "Debug", "netstandard2.0", "Content.dll")) $dependencies
+cp $([IO.PATH]::Combine("Library", "DeckbuilderLibrary", "Content", "bin", "Debug", "netstandard2.0", "Content.pdb")) $dependencies
+cp $([IO.PATH]::Combine("Library", "DeckbuilderLibrary", "DeckbuilderLibrary", "bin", "Debug", "netstandard2.0", "HexGrid.dll")) $dependencies
