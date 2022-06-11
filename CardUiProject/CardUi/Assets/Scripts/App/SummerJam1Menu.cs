eb31d2a5e127ff625c2b5a6048333fe927479491
Arthur <arthur.littlefield@gmail.com>
2022-06-11 11:24:53 -0700

    add missing

---
+++ CardUiProject/CardUi/Assets/Scripts/App/SummerJam1Menu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SummerJam1Menu : MonoBehaviour
+{
+  public void StartBattle()
+  {
+    SceneManager.LoadScene("Scenes/SummerJam1/BattleScene");
+  }
+
+ 
+}
