a0105e2fed940afe0f44c74ab0bb68197a1e7696
Arthur <arthur.littlefield@gmail.com>
2022-06-22 17:10:54 -0700

    add missing assets

---
+++ CardUiProject/CardUi/Assets/Scripts/SummerJam1/DamageSpawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace SummerJam1
+{
+    public class DamageSpawner : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
